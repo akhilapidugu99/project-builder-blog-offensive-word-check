9e639d8592b25be41867ec7830472062d20ee2c6
Akhila Pidugu <akhilapidugu99@gmail.com>
2021-01-21 21:22:24 +0530

    PROGRAD

--- src/utility/CheckBlogPost.java
+++ src/utility/CheckBlogPost.java
@@ -1,1 +1,46 @@
 package utility;
+
+import model.Blog;
+
+public class CheckBlogPost extends OffensiveWordsChecker implements OffensiveWordsInterface{
+	
+	String[] offensiveWords = {"ass", "bastard", "fuck", "anal", 
+			"asshole", "bondage","boned", "boner", "climax", "bong", 
+			"balls","cockmongruel", "boobs", "booby", "booger", "douche"
+			, "douchebag", "ahole", "douche-fag", "douchewaffle"
+			, "douchey", "drunk", "beaner"	, "cum", "bitch"};
+	
+
+	@Override
+	public boolean checkBlogTitle(Blog blog) {
+		// TODO Auto-generated method stub
+		String btitle = blog.getBlogTitle().toLowerCase();
+		for (String word : offensiveWords) {
+			if (btitle.contains(word)) {
+				return false;
+			}
+		}
+		return true;
+		
+	}
+
+	@Override
+	public boolean checkBlogDescription(Blog blog) {
+		// TODO Auto-generated method stub
+		String btitle = blog.getBlogDescription().toLowerCase();
+		for (String word : offensiveWords) {
+			if (btitle.contains(word)) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	@Override
+	public boolean checkBlog(Blog blog) {
+		// TODO Auto-generated method stub
+		return checkBlogTitle(blog) && checkBlogDescription(blog);
+	}
+	
+	
+}
